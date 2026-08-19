72828dcd75c9a767b722d468c7d149e819758c44
Henrique Guimarães Gonçalves <henrique.gg@dinfor.uniceub>
2026-08-19 10:15:26 -0300

    test: força erro no linter

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,12 +1,13 @@
+const x = 100; // Variável sem uso para forçar erro no ESLint
 function somar(a, b) {
-  return a + b;
+    return a + b;
 }
 
 function subtrair(a, b) {
-  return a - b;
+    return a - b;
 }
 
 module.exports = {
-  somar,
-  subtrair
+    somar,
+    subtrair
 };
